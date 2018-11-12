ebbfed6a611a0998f581cc22ad56c039b97b72a3
XavierDo <xdo@odoo.com>
2018-11-12 13:08:31 +0100

    some cleaing

--- wow/static/src/js/wowbot_service.js
+++ wow/static/src/js/wowbot_service.js
@@ -14,9 +14,7 @@ var WowBotService =  AbstractService.extend({
     start: function () {
         var self = this;
         if ('wow_initialized' in session && ! session.wow_initialized) {
-            console.log('start timeout');
             setTimeout(function () {
-                console.log('init_wow');
                 session.wow_initialized = true;
                 self._rpc({
                     model: 'mail.channel',
@@ -24,37 +22,6 @@ var WowBotService =  AbstractService.extend({
                 });
             }, 5*60*1000);
         }
-    },
-    /**
-     * Get the previews related to the OdooBot (conversation not included).
-     * For instance, when there is no conversation with OdooBot and OdooBot has
-     * a request, it should display a preview in the systray messaging menu.
-     *
-     * @param {string|undefined} [filter]
-     * @returns {Object[]} list of objects that are compatible with the
-     *   'mail.Preview' template.
-     */
-    getPreviews: function (filter) {
-        var previews = [];
-        if (this.hasRequest() && (filter === 'mailbox_inbox' || !filter)) {
-            previews.push({
-                title: _t("OdooBot has a request"),
-                imageSRC: "/mail/static/src/img/odoobot.png",
-                status: 'bot',
-                body:  _t("Enable desktop notifications to chat"),
-                id: 'request_notification',
-                unreadCounter: 1,
-            });
-        }
-        return previews;
-    },
-    /**
-     * Tell whether OdooBot has a request or not.
-     *
-     * @returns {boolean}
-     */
-    hasRequest: function () {
-        return window.Notification && window.Notification.permission === "default";
     },
 });
 
